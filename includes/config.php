450d2e32f5815788a5158e3b9c77b9e1b1efa92d
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-02-22 11:23:22 +0000

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -3,4 +3,12 @@
     if(!defined("BASE_URL"))
         define("BASE_URL", "https://phpwithmongodb.dev");
 
-    $pageName = basename($_SERVER["PHP_SELF"]);
+    // $pageName = basename($_SERVER["PHP_SELF"]);
+
+    function GenerateNewToken(){
+        return bin2hex(random_bytes(32));
+    }
+
+    function ExpirationTokenTime(){
+        return time() + 1800;    // 30 minutes
+    }
