2c7b2c104f5cbcef06ab41de9c0026eb4723b4c3
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-02-22 11:23:59 +0000

    Delete departments_script.js

--- transitionners/departments_script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(()=>{
-    $("#new-department-form").on("submit", (e)=>{
-        e.preventDefault()
-        let formData = $("#new-department-form").serialize()
-        console.log(formData)
-    })
-})
