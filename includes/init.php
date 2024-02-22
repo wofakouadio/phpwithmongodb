95a30280a063c38c083b3f648330b1d7cae0ad68
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-02-22 11:23:39 +0000

    Update init.php

--- includes/init.php
+++ includes/init.php
@@ -5,5 +5,3 @@ define("ABSOLUTE_PATH", $_SERVER["DOCUMENT_ROOT"]);
 
 require_once ABSOLUTE_PATH.'/constants/db_constants.php';
 require_once ABSOLUTE_PATH.'/controllers/database/DatabaseClass.php';
-require_once ABSOLUTE_PATH.'/controllers/authentication/AuthenticationClass.php';
-
