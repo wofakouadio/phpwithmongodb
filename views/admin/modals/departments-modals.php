833eff6e2806854bb38f3b6e1a7d735eb506c2ce
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-02-22 11:24:08 +0000

    Update departments-modals.php

--- views/admin/modals/departments-modals.php
+++ views/admin/modals/departments-modals.php
@@ -1,3 +1,6 @@
+<?php
+    require_once "../../../includes/TokenSession.php";
+?>
 <!-- create new department -->
 <div class="modal fade new-department-modal" tabindex="-1" role="dialog" aria-hidden="true">
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST" id="new-department-form">
@@ -11,6 +14,7 @@
                 <div class="modal-body">
                     <div class="col-xl-12 mb-4">
                         <label  class="form-label font-w600">Name<span class="text-danger scale5 ms-2">*</span></label>
+                        <input type="hidden" name="_token" value="<?php echo $_SESSION['token']; ?>">
                         <input type="text" class="form-control solid" placeholder="Name" aria-label="name" name="department-name">
                     </div>
                     <div class="col-xl-12 mb-4">
@@ -21,6 +25,63 @@
                 <div class="modal-footer">
                     <button type="button" class="btn btn-danger light" data-bs-dismiss="modal">Close</button>
                     <button type="submit" class="btn btn-primary">Save</button>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+
+<!-- edit department -->
+<div class="modal fade edit-department-modal" tabindex="-1" role="dialog" aria-hidden="true">
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST" id="edit-department-form">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Departments | Update</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal">
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="col-xl-12 mb-4">
+                        <label  class="form-label font-w600">Name<span class="text-danger scale5 ms-2">*</span></label>
+                        <input type="hidden" name="department-id">
+                        <input type="hidden" name="_token" value="<?php echo $_SESSION['token']; ?>">
+                        <input type="text" class="form-control solid" placeholder="Name" aria-label="name" name="department-name">
+                    </div>
+                    <div class="col-xl-12 mb-4">
+                        <label  class="form-label font-w600">Description</label>
+                        <textarea class="form-control solid" rows="5" aria-label="With textarea" name="department-description"></textarea>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger light" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Update</button>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+
+<!-- delete department -->
+<div class="modal fade delete-department-modal" tabindex="-1" role="dialog" aria-hidden="true">
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST" id="delete-department-form">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Departments | Delete</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal">
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="col-xl-12 mb-4">
+                        <label class="form-label font-w600" id="department-notice">Notice to delete selected department</label>
+                        <input type="hidden" name="department-id">
+                        <input type="hidden" name="_token" value="<?php echo $_SESSION['token']; ?>">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger light" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Delete</button>
                 </div>
             </div>
         </div>
