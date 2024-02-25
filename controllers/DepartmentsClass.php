1f0c73f4de079782e266bef650899bcac6a5406f
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-02-25 12:17:44 +0000

    Update DepartmentsClass.php

--- controllers/DepartmentsClass.php
+++ controllers/DepartmentsClass.php
@@ -20,6 +20,7 @@
         protected $deleted_at;
         protected $response = [];
 
+        // method to store new department
         public function NewDepartment($name, $description = ""){
             $this->id = $this->NewID();
             $this->name = $name;
@@ -29,9 +30,10 @@
             $connection = $this->OpenConnection();
 
             // check if department already exists
-            $sqlCheck = "SELECT `id`, `name` FROM `departments` WHERE `id` = :id LIMIT 1";
+            $sqlCheck = "SELECT `id`, `name` FROM `departments` WHERE `id` = :id OR `name` = :name LIMIT 1";
             $stmtCheck = $connection->prepare($sqlCheck);
             $stmtCheck->bindValue(":id", $this->id, PDO::PARAM_STR);
+            $stmtCheck->bindValue(":name", $this->name, PDO::PARAM_STR);
             $stmtCheck->execute();
 
             if($stmtCheck->rowCount() > 0){
@@ -45,7 +47,7 @@
                     $sqlIns = "INSERT INTO `departments` (`id`, `name`, `description`) VALUES(:id, :name, :description)";
                     $stmtIns = $connection->prepare($sqlIns);
                     $stmtIns->bindValue(":id", $this->id, PDO::PARAM_STMT);
-                    $stmtIns->bindValue(":name", ucfirst($name), PDO::PARAM_STMT);
+                    $stmtIns->bindValue(":name",$name, PDO::PARAM_STMT);
                     $stmtIns->bindValue(":description", $description, PDO::PARAM_STMT);
                     
                     $stmtIns->execute();
@@ -61,6 +63,81 @@
                         'msg' => 'Something went wrong. Error : ' . $th->getMessage()
                     ];
                 }
+            }
+            return json_encode($this->response);
+            $connection = $this->CloseConnection();
+        }
+
+        // method to fetch department based on department id
+        public function DepartmentData($id){
+            $this->id = $id;
+
+            // initiate db connection
+            $connection = $this->OpenConnection();
+
+            // check if department already exists
+            $sqlFetch = "SELECT `id`, `name`, `description`, `status` FROM `departments` WHERE `id` = :id LIMIT 1";
+            $stmtFetch = $connection->prepare($sqlFetch);
+            $stmtFetch->bindValue(":id", $this->id, PDO::PARAM_STR);
+            $stmtFetch->execute();
+
+            if($stmtFetch->rowCount() == 0){
+                $this->response = [
+                    'status' => 201,
+                    'msg' => 'The department does not exist'
+                ];
+            }else{
+
+                $DepartmentData = $stmtFetch->fetch(PDO::FETCH_OBJ);
+
+                $this->response = [
+                    'status' => 200,
+                    'msg' => 'Data found',
+                    'data' => [
+                        'id' => $id,
+                        'name' => $DepartmentData->name,
+                        'description' => $DepartmentData->description,
+                        'status' => $DepartmentData->status
+                    ]
+                ];
+                
+                
+            }
+            return json_encode($this->response);
+            $connection = $this->CloseConnection();
+        }
+
+        // method to update department
+        public function UpdateDepartment($id, $name, $description){
+            $this->id = $id;
+            $this->name = $name;
+            $this->description = $description;
+            $this->updated_at = date('Y-m-d H:i:s');
+
+            // initiate db connection
+            $connection = $this->OpenConnection();
+
+            // update department based on id
+            try {
+                $sqlUpd = "UPDATE `departments` SET `name` = :name , `description` = :description, `updated_at` = :updated_at WHERE `id` = :id";
+                $stmtUpd = $connection->prepare($sqlUpd);
+                $stmtUpd->bindValue(":id", $id, PDO::PARAM_STMT);
+                $stmtUpd->bindValue(":name",$name, PDO::PARAM_STMT);
+                $stmtUpd->bindValue(":description", $description, PDO::PARAM_STMT);
+                $stmtUpd->bindValue(":updated_at", $this->updated_at, PDO::PARAM_STMT);
+                
+                $stmtUpd->execute();
+
+                $this->response = [
+                    'status' => 200,
+                    'msg' => 'The department has been updated successfully'
+                ];
+
+            } catch (\Exception $th) {
+                $this->response = [
+                    'status' => 201,
+                    'msg' => 'Something went wrong. Error : ' . $th->getMessage()
+                ];
             }
             return json_encode($this->response);
             $connection = $this->CloseConnection();
