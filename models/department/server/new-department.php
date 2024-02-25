d4f2524435c4c074909f51f24ae5491abf23258e
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-02-25 12:18:36 +0000

    Update new-department.php

--- models/department/server/new-department.php
+++ models/department/server/new-department.php
@@ -2,35 +2,36 @@
 
     // session_start();
 
-    require "../../includes/TokenSession.php";
+    require_once "../../../includes/TokenSession.php";
 
-    if(isset($_POST["department-name"]) && isset($_POST["department-description"]) && $_POST["_token"] && isset($_SESSION["tokenexpire"]) && isset($_SESSION["token"])){
+    if(isset($_POST["department-name"]) && isset($_POST["department-description"]) && isset($_POST["_token"]) && isset($_SESSION["token"])){
 
         $department_name = filter_input(INPUT_POST, "department-name", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         $department_description = filter_input(INPUT_POST, "department-description", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         $formToken = filter_input(INPUT_POST, "_token", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
         if(time() > $_SESSION["tokenexpire"]){
+            session_destroy();
             $response = [
                 'status' => 400,
-                'msg' => "Token has expired"
+                'msg' => "Token has expired. Hot reload to refresh the session"
             ];
         }else{
             if($_SESSION["token"] ==! $formToken){
-
+                session_destroy();
                 $response = [
                     'status' => 400,
                     'msg' => "Token mismatch. Reload to regenerate new token"
                 ];
     
             }else{
-                require "../../includes/init.php";
-                require_once '../../includes/IdGenerator.php';
-                require_once '../../controllers/DepartmentsClass.php';
+                require "../../../includes/init.php";
+                require_once '../../../includes/IdGenerator.php';
+                require_once '../../../controllers/DepartmentsClass.php';
         
                 $DepartmentObject = new DepartmentsClass();
         
-                $NewDepartment = $DepartmentObject->NewDepartment($department_name, $department_description);
+                $NewDepartment = $DepartmentObject->NewDepartment(strtoupper($department_name), $department_description);
         
                 $response = json_decode($NewDepartment);
             }
