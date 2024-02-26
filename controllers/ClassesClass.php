2406c22e4c1901e15cb4876457b020acac2fd07d
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-02-26 19:29:07 +0000

    Update ClassesClass.php

--- controllers/ClassesClass.php
+++ controllers/ClassesClass.php
@@ -17,6 +17,7 @@ class ClassesClass{
     protected $deleted_at;
     protected $response = [];
 
+    // method to create new class room
     public function NewClass($name){
         $this->id = $this->NewID();
         $this->name = $name;
@@ -57,6 +58,139 @@ class ClassesClass{
                     'msg' => 'Something went wrong. Error : ' . $th->getMessage()
                 ];
             }
+        }
+        return json_encode($this->response);
+        $connection = $this->CloseConnection();
+    }
+
+    // method to fetch class room data
+    public function ClassData($id){
+        $this->id = $id;
+
+        // initiate db connection
+        $connection = $this->OpenConnection();
+
+        // check if department already exists
+        $sqlFetch = "SELECT `id`, `name`, `status` FROM `classes` WHERE `id` = :id LIMIT 1";
+        $stmtFetch = $connection->prepare($sqlFetch);
+        $stmtFetch->bindValue(":id", $this->id, PDO::PARAM_STR);
+        $stmtFetch->execute();
+
+        if($stmtFetch->rowCount() == 0){
+            $this->response = [
+                'status' => 201,
+                'msg' => 'The class does not exist'
+            ];
+        }else{
+
+            $ClassData = $stmtFetch->fetch(PDO::FETCH_OBJ);
+
+            $this->response = [
+                'status' => 200,
+                'msg' => 'Data found',
+                'data' => [
+                    'id' => $id,
+                    'name' => $ClassData->name,
+                    'status' => $ClassData->status
+                ]
+            ];
+            
+        }
+        return json_encode($this->response);
+        $connection = $this->CloseConnection();
+    }
+
+    // method to update class room data
+    public function UpdateClass($id, $name, $status){
+        $this->id = $id;
+        $this->name = $name;
+        $this->status = $status;
+        $this->updated_at = date('Y-m-d H:i:s');
+
+        // initiate db connection
+        $connection = $this->OpenConnection();
+
+        // update department based on id
+        try {
+            $sqlUpd = "UPDATE `classes` SET `name` = :name , `status` = :status, `updated_at` = :updated_at WHERE `id` = :id";
+            $stmtUpd = $connection->prepare($sqlUpd);
+            $stmtUpd->bindValue(":id", $id, PDO::PARAM_STMT);
+            $stmtUpd->bindValue(":name",$name, PDO::PARAM_STMT);
+            $stmtUpd->bindValue(":status", $status, PDO::PARAM_INT);
+            $stmtUpd->bindValue(":updated_at", $this->updated_at, PDO::PARAM_STMT);
+            
+            $stmtUpd->execute();
+
+            $this->response = [
+                'status' => 200,
+                'msg' => 'The class has been updated successfully'
+            ];
+
+        } catch (\Exception $th) {
+            $this->response = [
+                'status' => 201,
+                'msg' => 'Something went wrong. Error : ' . $th->getMessage()
+            ];
+        }
+        return json_encode($this->response);
+        $connection = $this->CloseConnection();
+    }
+
+    // method to delete data by adding timestamp
+    public function SoftDeleteClassData($id){
+        $this->id = $id;
+        $this->deleted_at = date('Y-m-d H:i:s');
+
+        // initiate db connection
+        $connection = $this->OpenConnection();
+
+        try {
+            $sqlUpd = "UPDATE `classes` SET `deleted_at` = :deleted_at WHERE `id` = :id";
+            $stmtUpd = $connection->prepare($sqlUpd);
+            $stmtUpd->bindValue(":id", $id, PDO::PARAM_STMT);
+            $stmtUpd->bindValue(":deleted_at", $this->deleted_at, PDO::PARAM_STMT);
+            
+            $stmtUpd->execute();
+
+            $this->response = [
+                'status' => 200,
+                'msg' => 'The class has been deleted successfully'
+            ];
+
+        } catch (\Exception $th) {
+            $this->response = [
+                'status' => 201,
+                'msg' => 'Something went wrong. Error : ' . $th->getMessage()
+            ];
+        }
+        return json_encode($this->response);
+        $connection = $this->CloseConnection();
+    }
+
+    // method to delete data completely from the db
+    public function DeleteClassData($id){
+        $this->id = $id;
+
+        // initiate db connection
+        $connection = $this->OpenConnection();
+
+        try {
+            $sqlUpd = "DELETE FROM `classes` WHERE `id` = :id";
+            $stmtUpd = $connection->prepare($sqlUpd);
+            $stmtUpd->bindValue(":id", $id, PDO::PARAM_STMT);
+            
+            $stmtUpd->execute();
+
+            $this->response = [
+                'status' => 200,
+                'msg' => 'The class has been deleted successfully'
+            ];
+
+        } catch (\Exception $th) {
+            $this->response = [
+                'status' => 201,
+                'msg' => 'Something went wrong. Error : ' . $th->getMessage()
+            ];
         }
         return json_encode($this->response);
         $connection = $this->CloseConnection();
