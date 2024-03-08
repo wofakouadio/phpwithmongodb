2afd0301126600fbc1563307faa6d3f77b7ea9d5
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-03-08 12:05:38 +0000

    Update get_department_in_dropdown_script.js

--- models/functions/js/get_department_in_dropdown_script.js
+++ models/functions/js/get_department_in_dropdown_script.js
@@ -1,10 +1,9 @@
 $(document).ready(()=>{
 
+    // function to fetch department in select form input type
     const GetDepartmentInDropdown = () =>{
         $.ajax({
             url:'../../models/functions/server/get_department_in_dropdown.php',
-            method: 'GET',
-            cache:false,
             success: (Response)=>{
                 $("#new-teacher-form select[name=teacher-department]").html(Response);
             }
